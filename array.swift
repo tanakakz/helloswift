0f7ebc71bb429bb9cd6eda9a6db4801b4cbf981a
tanakakz <tanakakz.feb.23@gmail.com>
2020-02-14 22:46:33 +0900

    array lesson

---
+++ array.swift
@@ -0,0 +1,13 @@
+
+var a1 = [ 2, 5, 8, 11, 7 ]
+var a2 : [Int] = [ 1, 3, 5, 7, 9 ]
+print(a1, a2)
+
+var s1 = [String]()
+var s2 : [String] = []
+print(s1, s2)
+
+var g = 1.2
+var f = [ g/2.0, g/0.3, g/0.04 ]
+print(f)
+
