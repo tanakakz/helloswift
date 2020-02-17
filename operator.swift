04c874645b410f171e7d22384030e54c344cd675
tanakakz <tanakakz.feb.23@gmail.com>
2020-02-17 21:32:21 +0900

    operator lesson

---
+++ operator.swift
@@ -0,0 +1,16 @@
+
+var a: Int = 1
+// a++
+a += 1
+print(a)
+
+var b: Int = 3
+// a = b = 1
+
+// if a = 1 { print(a) }
+if a == 2 { print("a == 2") }
+
+// print(a+ b)
+// print(a +b)
+print(a+b)
+print(a + b)
