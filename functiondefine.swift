a045e12133bf855fcd01c55c1d450c0b5b69fa68
tanakakz <tanakakz.feb.23@gmail.com>
2020-03-10 22:19:36 +0900

    func define lesson

---
+++ functiondefine.swift
@@ -0,0 +1,16 @@
+var total = 0
+
+func count(n: Int) -> Int {
+ total += n
+ return total 
+}
+
+func reset() { total = 0 }
+
+reset()
+let a = 10
+let b = count(n: a - 5)
+print("\(b)")
+print("\(count(n: a))")
+reset()
+print("\(count(n: -2))")
