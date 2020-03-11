7a35477dbc90d7648b42ca1cb6d5014ab3a3144f
tanakakz <tanakakz.feb.23@gmail.com>
2020-03-11 21:09:08 +0900

    func call with arg label

--- functiondefine.swift
+++ functiondefine.swift
@@ -14,3 +14,9 @@ print("\(b)")
 print("\(count(n: a))")
 reset()
 print("\(count(n: -2))")
+
+func buy(product:Int, price:Int, quantity:Int) {
+  print("Product:\(product), amount = \(price * quantity)")
+}
+
+buy(product:19090, price:180000, quantity:1)
