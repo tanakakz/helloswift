aa81caa94264f1e39d061762e32aabffa0825a82
tanakakz <tanakakz.feb.23@gmail.com>
2020-03-13 21:08:50 +0900

    func arg label

--- functiondefine.swift
+++ functiondefine.swift
@@ -28,3 +28,9 @@ func messageB() -> String { "現在の値は\(total)です。" }
 
 print(messageA())
 print(messageB())
+
+func area(h:Double, w:Double) -> Double {
+  return h * w
+}
+let here = area(h:10.0, w:12.5)
+print(here)
