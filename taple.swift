393018b39c161f4eeba1075e4fb7f9437f9fab72
tanakakz <tanakakz.feb.23@gmail.com>
2020-03-25 21:05:43 +0900

    same and diff type taple

--- taple.swift
+++ taple.swift
@@ -1,2 +1,6 @@
 let b = ("bird.jpg", 161_022)
 let c : (String, Int) = ("cat.jpg", 234_341)
+
+let cat = ("cat.jpg", 1024, 768)
+var img : (String, Int, Int) = cat
+// img = c // Error. different type
