3f3fee297c70e97a796dc6f3acf337b8f5ca5cc8
tanakakz <tanakakz.feb.23@gmail.com>
2020-03-26 22:49:18 +0900

    print taple value

--- taple.swift
+++ taple.swift
@@ -4,3 +4,7 @@ let c : (String, Int) = ("cat.jpg", 234_341)
 let cat = ("cat.jpg", 1024, 768)
 var img : (String, Int, Int) = cat
 // img = c // Error. different type
+
+print("\(img.0): \(img.1)x\(img.2)")
+img.2 = 800
+print("\(img.1)x\(img.2)")
