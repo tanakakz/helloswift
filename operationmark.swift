0b341f710f13bbff27398f43e0195b22f02671c2
tanakakz <tanakakz.feb.23@gmail.com>
2020-04-07 23:36:31 +0900

    add original operator

--- operationmark.swift
+++ operationmark.swift
@@ -18,3 +18,21 @@ for n in [ 2, 8, 10, -1 ] {
 var z: UInt8 = 1
 z = z &- 2 // z == 255. if calc z - 2 then error.
 print(z)
+
+infix operator 〜 : RangeFormationPrecedence // 二項演算子として宣言
+
+func 〜 (n:Int, w:Int) -> String {
+  var str = String(n)
+  let pad = w - str.count
+  if pad > 0 {
+    str = String(repeating:" ", count:pad) + str
+  }
+  return str
+}
+
+for wid in [ 2, 4, 6 ] {
+  for i in 1...5 {
+    print(i + 7 〜 wid, terminator:"")
+  }
+  print()
+}
