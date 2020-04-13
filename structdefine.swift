6bdfe949cdfa391d37df71516d7f9c3b14101f47
tanakakz <tanakakz.feb.23@gmail.com>
2020-04-13 22:52:28 +0900

    let in struct

--- structdefine.swift
+++ structdefine.swift
@@ -7,3 +7,17 @@ struct SimpleDate {
 let d = SimpleDate(year: 2010, month: 7, day: 28)
 print(d)
 // d.day = 10 // cannot it. "d" is imutable instance.
+
+struct Time12h {
+  let in24h: Bool = false
+  var hour = 0, min = 0
+}
+var t1 = Time12h()
+// var t2 = Time12h(in24h:true, hour:7, min:0) // will failure.
+var t2 = Time12h(hour:7, min:0)
+
+struct Time {
+  let in24h: Bool
+  var hour = 0, min = 0
+}
+var t3 = Time(in24h:true, hour:7, min:0)
