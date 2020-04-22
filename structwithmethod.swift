0a201cccb9ceaa33b0c733d4e985718dad3bb08f
tanakakz <tanakakz.feb.23@gmail.com>
2020-04-22 20:12:38 +0900

    struct mutating func

--- structwithmethod.swift
+++ structwithmethod.swift
@@ -20,3 +20,29 @@ let t1 = Time(hour:22, min:45)
 let t2 = t1.advanced(min:140)
 print(t1.toString())
 print(t2.toString())
+
+struct Clock {
+  var hour = 0, min = 0
+  mutating func advance(min:Int) {
+    let m = self.min + min
+    if m >= 60 {
+      self.min = m % 60
+      let t = self.hour + m / 60
+      self.hour = t % 24
+    } else {
+      self.min = m
+    }
+  }
+  mutating func inc() {
+    self.advance(min: 1)
+  }
+  func toString() -> String {
+    let h = hour < 10 ? " \(hour)" : "\(hour)"
+    let m = min < 10 ? "0\(min)" : "\(min)"
+    return h + ":" + m
+  }
+}
+var tic = Clock(hour:19, min:40)
+tic.advance(min:19)
+tic.inc()
+print(tic.toString())
