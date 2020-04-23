4b258f27387123816cabe7c7b82e198e5e28888c
tanakakz <tanakakz.feb.23@gmail.com>
2020-04-23 19:05:37 +0900

    type method

--- structwithmethod.swift
+++ structwithmethod.swift
@@ -46,3 +46,25 @@ var tic = Clock(hour:19, min:40)
 tic.advance(min:19)
 tic.inc()
 print(tic.toString())
+
+struct SimpleDate {
+  var year, month, day: Int
+  static 
+  func isLeap(_ y: Int) -> Bool {
+    return (y % 4 == 0) && (y % 100 != 0 || y % 400 == 0)
+  }
+  static func daysOfMonth(_ m:Int, year:Int) -> Int {
+    switch m {
+    case 2: return isLeap(year) ? 29 : 28
+    case 4, 6, 9, 11: return 30
+    default: return 31
+    }
+  }
+  func leapYear() -> Bool {
+    SimpleDate.isLeap(year)
+  }
+}
+print(SimpleDate.isLeap(2000))
+print(SimpleDate.daysOfMonth(2, year:2000))
+let d = SimpleDate(year:2024, month:11, day:7)
+print("うるう年？ ", d.leapYear())
