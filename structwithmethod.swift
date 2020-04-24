3fae8f6403d63cd339737fa92628931d0c6582a5
tanakakz <tanakakz.feb.23@gmail.com>
2020-04-24 22:46:06 +0900

    initializer with method

--- structwithmethod.swift
+++ structwithmethod.swift
@@ -68,3 +68,20 @@ print(SimpleDate.isLeap(2000))
 print(SimpleDate.daysOfMonth(2, year:2000))
 let d = SimpleDate(year:2024, month:11, day:7)
 print("うるう年？ ", d.leapYear())
+
+struct DateWithString {
+  let string: String
+  let year, month, day: Int
+  init(_ y:Int, _ m:Int, _ d:Int) {
+    year = y; month = m; day = d
+    string = "\(y)-" + DateWithString.twoDigits(m) + "-" + DateWithString.twoDigits(d)
+  }
+  static func twoDigits(_ n:Int) -> String {
+    let i = n % 100
+    return i < 10 ? "0\(i)" : "\(i)"
+  }
+}
+let an1 = DateWithString(2015, 6, 22)
+print(an1.string)
+
+
