b8a6911940d379feb720bbc98e340b9d61839b78
tanakakz <tanakakz.feb.23@gmail.com>
2020-04-25 11:05:29 +0900

    tic tac

--- structwithmethod.swift
+++ structwithmethod.swift
@@ -84,4 +84,37 @@ struct DateWithString {
 let an1 = DateWithString(2015, 6, 22)
 print(an1.string)
 
-
+struct Clock2 {
+  var hour = 0, min = 0
+  mutating func advance(min:Int) {
+    let m = self.min + min
+    if m >= 60 {
+      self.min = m % 60
+      let t = self.hour + m / 60
+      self.hour = t % 24
+    } else {
+      self.min = m
+    }
+  }
+  mutating func inc() {
+    self.advance(min: 1)
+  }
+  func toString() -> String {
+    let h = hour < 10 ? " \(hour)" : "\(hour)"
+    let m = min < 10 ? "0\(min)" : "\(min)"
+    return h + ":" + m
+  }
+  static func +(lhs: Clock2, rhs: Int) -> Clock2 {
+    var t = lhs
+    t.advance(min: rhs)
+    return t
+  }
+  static func +=(lhs: inout Clock2, rhs: Int) {
+    lhs.advance(min:rhs)
+  }
+}
+let tic Clock(hour:19, min:40)
+var tac = tic + 75
+print(tac.toString())
+tac += 125
+print(tac.toString())
