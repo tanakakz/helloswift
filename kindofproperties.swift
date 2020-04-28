df87722508a8f2774b472c0da26ca8d5b77b0dda
tanakakz <tanakakz.feb.23@gmail.com>
2020-04-28 23:28:30 +0900

    stored property

--- kindofproperties.swift
+++ kindofproperties.swift
@@ -30,3 +30,31 @@ DateWithString.longFormat = true
 
 let b = DateWithString(2025, 1, 21)
 print(b.string)
+
+// stored property
+var serialNumber = 2127
+
+struct LCD {
+  struct Size { var width, height : Int }
+  static var stdHeight = 1080
+  static var stdWidth = 1920
+  static var stdSize = Size(width: stdWidth, height: stdHeight)
+  static func sn() -> Int { serialNumber += 1; return serialNumber }
+  let size: Size
+  let serial = "CZ:" + String(LCD.sn())
+  
+  init(size: Size = LCD.stdSize) {
+    self.size = size
+  }
+  func show() {
+    print(serial, "(\(size.width)x\(size.height))")
+  }
+}
+let display1 = LCD(size: LCD.Size(width:800, height:600))
+display1.show()
+LCD.stdHeight = 1200
+let display2 = LCD()
+display2.show()
+LCD.stdWidth = 2560
+let display3 = LCD()
+display3.show()
