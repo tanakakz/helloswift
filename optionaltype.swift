21eae5539478e1e7e95979e5bc72f01b908771b9
tanakakz <tanakakz.feb.23@gmail.com>
2020-05-12 18:16:03 +0900

    optional type lesson3

--- optionaltype.swift
+++ optionaltype.swift
@@ -12,3 +12,6 @@ var year2 : Optional<Int> = Int("2020")
 
 let year3 : Int? = Int("令和20年") // year is nil
 // let next3 : Int = year3! + 4 // will error.
+
+var year4 : Int? = Int("2020")
+year4! += 4 // need publication. year is 2024
