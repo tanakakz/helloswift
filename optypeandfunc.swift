93c386d64c8932fd4b9464c08c82bd17983936c9
tanakakz <tanakakz.feb.23@gmail.com>
2020-05-22 23:57:41 +0900

    use optional to inout arg in func

--- optypeandfunc.swift
+++ optypeandfunc.swift
@@ -4,3 +4,7 @@ func nickname(_ name:String?, age:Int) -> String {
 }
 print(nickname("sinderera", age:35))
 print(nickname(nil, age:20))
+
+func makeNil(_ p: inout String?) {
+  if let s = p, s == @@ { p = nil }
+}
